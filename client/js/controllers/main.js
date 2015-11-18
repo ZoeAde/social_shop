8f624a3fb0c93bb63204f5720c90afdedd70bf14
ZoeAde <zoe.a.adelman@gmail.com>
2015-11-18 16:19:22 -0700

    setting up instagram feed

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -1,19 +1,20 @@
-app.controller('mainController', function($scope, myFactory, $http, $location, $routeParams, $filter, $document, $window, $auth, $rootScope, $anchorScroll){
+app.controller('mainController', function($scope, myFactory, $http, $interval, instagram, $location, $routeParams, $filter, $document, $window, $auth, $rootScope, $anchorScroll){
 
   $anchorScroll();
-  $scope.token = "";
+
   $scope.isAuthenticated = function() {
     return $auth.isAuthenticated();
   };
-$scope.authenticate = function(provider) {
+
+  $scope.authenticate = function(provider) {
 
     $auth.authenticate(provider)
       .then(function(response) {
         $window.localStorage.currentUser = JSON.stringify(response.data.user);
         $rootScope.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        console.log($rootScope.currentUser.isFulfilled);
         $rootScope.user = response.data.user;
-        console.log('user is...', $rootScope.user);
+        $rootScope.token = response.data.token;
+        console.log($rootScope.token);
         $location.path('/home');
       })
     .catch(function(response) {
@@ -26,13 +27,9 @@ $scope.authenticate = function(provider) {
     delete $window.localStorage.currentUser;
   };
 
-  // $scope.toTheTop = function() {
-  //   $document.scrollTopAnimated(0, 1400).then(function() {
-  //   });
-  // };
-
   // go = function(marker){
   //   $location.path(marker);
   // };
+
 
 });
