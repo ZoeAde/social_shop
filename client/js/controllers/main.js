a5cd00e1e84a1df15a1005da176ee36b1067a6e4
ZoeAde <zoe.a.adelman@gmail.com>
2015-11-23 17:46:00 -0700

    adding materialize formatting

--- client/js/controllers/main.js
+++ client/js/controllers/main.js
@@ -1,9 +1,6 @@
 app.controller('mainController', ['$scope', 'instagram', 'myFactory', '$http', '$interval', '$location', '$routeParams', '$filter', '$document', '$window', '$auth', '$rootScope', function($scope, instagram, myFactory, $http, $interval, $location, $routeParams, $filter, $document, $window, $auth, $rootScope){
 
   $scope.getFeed = function() {
-    // var instagramUrl = 'https://api.instagram.com/v1/users/' + instagram_id + '/media/recent/?client_id=311624c2f9f9454a9c7c053b234cc12a';
-    console.log('inside get feed!');
-
     $http.get('http://localhost:5000/auth/api/feed').then(function (response) {
       $scope.photos = response.data;
     });
@@ -11,15 +8,13 @@ app.controller('mainController', ['$scope', 'instagram', 'myFactory', '$http', '
 
   $scope.imageInfo = function() {
     $scope.newItemUrl = this.photo;
-    console.log($scope.newItemUrl);
-  }
+  };
 
   $scope.isAuthenticated = function() {
     return $auth.isAuthenticated();
   };
 
   $scope.authenticate = function(provider) {
-
     $auth.authenticate(provider)
       .then(function(response) {
         $window.localStorage.currentUser = JSON.stringify(response.data.user);
@@ -31,13 +26,11 @@ app.controller('mainController', ['$scope', 'instagram', 'myFactory', '$http', '
       .catch(function(response) {
         console.log(response);
       });
-    };
+  };
 
   $scope.logout = function() {
     $auth.logout();
     delete $window.localStorage.currentUser;
   };
-
-
 
 }]);
